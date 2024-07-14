8f506ecc6281fed176174e819212af5884a37a76
230590R <230590R@gmail.com>
2024-07-14 14:15:43 +0800

    penguin cover

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ class MusicLayer {
 
 
 // define buttons
-let btnStart = document.getElementById("start");
-let btnMusic = document.getElementById("music");
-let btnBass = document.getElementById("bass");
-let btnDrums = document.getElementById("drums");
+// let btnStart = document.getElementById("start");
+// let btnMusic = document.getElementById("music");
+// let btnBass = document.getElementById("bass");
+// let btnDrums = document.getElementById("drums");
 
 let btnHam = document.getElementById("hambtn");
 
@@ -26,20 +26,20 @@ let drums = new MusicLayer("audio/bgm-drums.mp3", 0, 0);
 
 let hamicon = document.querySelector(".hamicon");
 
-btnStart.addEventListener('click', () => {
-  music.audio.play();
-  bass.audio.play();
-  drums.audio.play();
-})
+// btnStart.addEventListener('click', () => {
+//   music.audio.play();
+//   bass.audio.play();
+//   drums.audio.play();
+// })
 
 btnHam.addEventListener('click', () => {
   hamicon.classList.toggle("hamicon-toggled");
 })
 
 
-btnMusic.addEventListener('click', function() { ToggleAudio(music, 0.25); });
-btnBass.addEventListener('click', function() { ToggleAudio(bass, 1); });
-btnDrums.addEventListener('click', function() { ToggleAudio(drums, 0.25); });
+// btnMusic.addEventListener('click', function() { ToggleAudio(music, 0.25); });
+// btnBass.addEventListener('click', function() { ToggleAudio(bass, 1); });
+// btnDrums.addEventListener('click', function() { ToggleAudio(drums, 0.25); });
 
 function ToggleAudio(a, volume) {
   a.maxvolume = (a.maxvolume == 0)
@@ -59,19 +59,9 @@ function UpdateLoop(dt) {
   music.Update(dt);
   bass.Update(dt);
   drums.Update(dt);
-  let dtelement = document.getElementById("dt");
-  dtelement.innerHTML = dt;
 }
 
 
 
 
 requestAnimationFrame(AppLoop);
-
-//alert("bozo");
-
-// while (true) {
-//   music.Update();
-//   bass.Update();
-//   drums.Update();
-// }
