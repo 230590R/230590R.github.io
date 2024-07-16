28303bd6c203a4b9ad362352da3703da4dd65ae7
230590R <230590R@gmail.com>
2024-07-16 13:31:12 +0800

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 class MusicLayer {
   constructor(filepath, maxvolume, volume = 0) {
     this.audio = new Audio(filepath);
@@ -12,11 +13,38 @@ class MusicLayer {
 }
 
 
+
+// class to encapsulate the cover parallax
+class Cover {
+  constructor() {
+    this.mountain = document.getElementById("title-mountain");
+    this.ground = document.getElementById("title-ground");
+    this.text = document.getElementById("title-h1");
+    this.cover = document.getElementById("title-cover");
+    
+    // define the position of the title, set it
+    this.textTop = -250;
+    this.text.style.top = this.textTop + 'px';
+  }
+  ScrollUpdate(scrollY) {
+    this.mountain.style.top = (scrollY * -0.75) + 'px';
+    this.cover.style.top = (scrollY * -1) + 'px';
+    let textPos = this.textTop + (scrollY * 0.005 * Math.abs(this.textTop));
+    this.text.style.top = Math.min(textPos, 200) + 'px';
+  }
+}
+
+let cover = new Cover();
+window.addEventListener('scroll', function () {
+  cover.ScrollUpdate(window.scrollY);
+});
+
+
 // define buttons
-// let btnStart = document.getElementById("start");
-// let btnMusic = document.getElementById("music");
-// let btnBass = document.getElementById("bass");
-// let btnDrums = document.getElementById("drums");
+let btnStart = document.getElementById("start");
+let btnMusic = document.getElementById("music");
+let btnBass = document.getElementById("bass");
+let btnDrums = document.getElementById("drums");
 
 let btnHam = document.getElementById("hambtn");
 
@@ -26,29 +54,27 @@ let drums = new MusicLayer("audio/bgm-drums.mp3", 0, 0);
 
 let hamicon = document.querySelector(".hamicon");
 
-// btnStart.addEventListener('click', () => {
-//   music.audio.play();
-//   bass.audio.play();
-//   drums.audio.play();
-// })
+btnStart.addEventListener('click', () => {
+  music.audio.play();
+  bass.audio.play();
+  drums.audio.play();
+});
 
 btnHam.addEventListener('click', () => {
   hamicon.classList.toggle("hamicon-toggled");
-})
+});
 
 
-// btnMusic.addEventListener('click', function() { ToggleAudio(music, 0.25); });
-// btnBass.addEventListener('click', function() { ToggleAudio(bass, 1); });
-// btnDrums.addEventListener('click', function() { ToggleAudio(drums, 0.25); });
+btnMusic.addEventListener('click', function() { ToggleAudio(music, 0.25); });
+btnBass.addEventListener('click', function() { ToggleAudio(bass, 1); });
+btnDrums.addEventListener('click', function() { ToggleAudio(drums, 0.25); });
 
 function ToggleAudio(a, volume) {
-  a.maxvolume = (a.maxvolume == 0)
-    ? volume  
-    : 0;
+  a.maxvolume = (a.maxvolume == 0) ? volume : 0;
 }
 
 const deltatime = 0.006;
-function AppLoop(timestamp) {
+function AppLoop() {
   UpdateLoop(deltatime);
   // step into the next frame when frame time has elapsed
   requestAnimationFrame(AppLoop);
@@ -66,15 +92,3 @@ function UpdateLoop(dt) {
 
 requestAnimationFrame(AppLoop);
 
-
-
-let mountain = document.getElementById("title-mountain");
-let ground = document.getElementById("title-ground");
-let text = document.getElementById("title-h1");
-let cover = document.getElementById("title-cover");
-window.addEventListener('scroll', function() {
-  var value = window.scrollY;
-  mountain.style.top = (value * -0.5) + 'px';
-  cover.style.top = (value * -1) + 'px';
-  text.style.top =  -200 + (value * 0.5) + 'px';
-})
